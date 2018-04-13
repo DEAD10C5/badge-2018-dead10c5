4d902095db9a3e2302e18dd11c6d6caa71644a70
p0lr <p0lr@p0lr.org>
2018-04-12 20:57:33 -0600

    Updated arduino sketch to sweep LEDs more accurately.

--- arduino_sketches/deadlocks/deadlocks.ino
+++ arduino_sketches/deadlocks/deadlocks.ino
@@ -8,11 +8,13 @@ int leds[6] = {led1, led2, led3, led4, led5, led6};
 
 int button1 = 2;
 
-int pot1 = A5;
-int pot2 = A4;
-int pot3 = A3;
-int pot4 = A2;
+int pot1 = A2;
+int pot2 = A3;
+int pot3 = A4;
+int pot4 = A5;
 int pots[4] = {pot1, pot2, pot3, pot4};
+
+int combos[6][4];
 
 void setup() {
   Serial.begin(115200);
@@ -25,6 +27,15 @@ void setup() {
   for (int potCounter = 0; potCounter <=3; potCounter += 1) {
     pinMode(pots[potCounter], INPUT);
   }
+
+  int randSeed(analogRead(A0)*10);
+  randomSeed(randSeed);
+  for (int firstCount = 0; firstCount < 6; firstCount += 1) {
+    for (int secondCount = 0; secondCount < 4; secondCount += 1) {
+      combos[firstCount][secondCount] = random(1024);
+    }
+  }
+  
   pinMode(button1, INPUT_PULLUP);
   Serial.print("pots: ");
   Serial.print(sizeof(pots) / sizeof(int));
@@ -32,6 +43,61 @@ void setup() {
   Serial.print("leds: ");
   Serial.print(sizeof(leds) / sizeof(int));
   Serial.print("\n");
+  Serial.print("The random seed is: ");
+  Serial.print(randSeed);
+  Serial.print("\n");
+  for (int firstCount = 0; firstCount < 6; firstCount += 1) {
+    for (int secondCount = 0; secondCount < 4; secondCount += 1) {
+      Serial.print("Combination ");
+      Serial.print(firstCount);
+      Serial.print(", position ");
+      Serial.print(secondCount);
+      Serial.print(": ");
+      Serial.print(combos[firstCount][secondCount]);
+      Serial.print("\n");
+    }
+  }  
+}
+
+void sweepRight() {
+  int ledCount = (sizeof(leds) / sizeof(int));
+  for (int ledCounter = 0; ledCounter < ledCount;  ledCounter += 1) {
+    for (int fadeValue = 0 ; fadeValue <= 100; fadeValue += 10) {
+      analogWrite(leds[ledCounter], fadeValue);
+      delay(3);
+    }
+  }
+  for (int ledCounter = 0; ledCounter < ledCount;  ledCounter += 1) {
+    for (int fadeValue = 100 ; fadeValue >= 0; fadeValue -= 10) {
+      analogWrite(leds[ledCounter], fadeValue);
+      delay(3);
+    }
+  }  
+}
+
+void sweepLeft() {
+  int ledCount = (sizeof(leds) / sizeof(int));
+  for (int ledCounter = (ledCount-1); ledCounter >= 0;  ledCounter -= 1) {
+    for (int fadeValue = 0 ; fadeValue <= 100; fadeValue += 10) {
+      analogWrite(leds[ledCounter], fadeValue);
+      delay(3);
+    }
+  }
+  for (int ledCounter = ledCount; ledCounter >= 0;  ledCounter -= 1) {
+    for (int fadeValue = 100 ; fadeValue >= 0; fadeValue -= 10) {
+      analogWrite(leds[ledCounter], fadeValue);
+      delay(3);
+    }
+  }
+}
+
+void blinkLed(int led, int blinks) {
+  for (int flashCounter = 1 ; flashCounter <= blinks; flashCounter += 1) {
+    analogWrite(led, 255);
+    delay(100);
+    analogWrite(led, 0);
+    delay(100);
+  }
 }
 
 void loop() {
@@ -49,17 +115,8 @@ void loop() {
           Serial.print(potRead[potCounter]);
           Serial.print("\n");
     }
-    for (int ledCounter = 0; ledCounter < ledCount;  ledCounter += 1) {
-      for (int fadeValue = 0 ; fadeValue <= 100; fadeValue += 10) {
-        analogWrite(leds[ledCounter], fadeValue);
-        delay(5);
-      }
-    }
-    for (int ledCounter = 0; ledCounter < ledCount;  ledCounter += 1) {
-      for (int fadeValue = 100 ; fadeValue >= 0; fadeValue -= 10) {
-        analogWrite(leds[ledCounter], fadeValue);
-        delay(5);
-      }
-    }
+    sweepRight();
+    sweepLeft();
+    blinkLed(led1, 5);
   } 
 }
